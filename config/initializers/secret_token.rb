c293a1b3eba5285a07194296fafd6a8338b501bb
Oleg Trofimov + Ryan Urabe <apprentice+ot+ru@devbootcamp.com>
2012-08-01 10:04:47 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basecampclone::Application.config.secret_token = '1943b6ec84979fc2fe83532d689cda2edffbf283d1c62528ffc5bfe059838ca1d87e4db6989d99f91f91b85ccb224291758511047da53579c9fc7099dae33741'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Basecampclone::Application.config.session_store :cookie_store, key: '_basecampclone_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Basecampclone::Application.config.session_store :active_record_store
